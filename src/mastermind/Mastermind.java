b31db68904a1079fe49ecd9a88e7ee773ec9a2c9
DinhmenhNguyen <1213821@student.roc-nijmegen.nl>
2025-12-04 12:08:12 +0100

    Introductie verbeterd, alles naar engels gemaakt, for each gebruikt

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -5,103 +5,100 @@ import java.util.*;
 public class Mastermind {
 
 	public static void main(String[] args) {
-		System.out.println("Welkom bij mastermind!" + " \nJe moet de code raden met de volgende kleuren:"
-				+ " \nRood, Oranje, Blauw, Groen, Paars of Geel." + " \nBegin hier:"); // Introductie van de
-																						// spel/spelregels
-
-		Scanner sc = new Scanner(System.in); // De User input geven
-		Random rd = new Random(); // Een random generator in het progamma maken
-		// Codebreker
-		String[] codeKrakersKleuren = { "Rood", "Oranje", "Blauw", "Groen", "Paars", "Geel" }; // De kleuren voor de
-																								// Code en Codekraker
-																								// geven
+		System.out.print("Welcome to Mastermind!" + " \nTry and guess the color code with the following colors:"
+				+ " \nRed, Orange, Blue, Green, Purple or Yellow." + " \nEnter here:");
+		
+		String[] codeGuesserColors = { "Red", "Orange", "Blue", "Green", "Purple", "Yellow" };
 
 		// Codemaker
-		String[] codeMakersKleuren = { "Wit", "Zwart", "Leeg" }; // De kleuren voor de Codemaker geven
+		String[] codeMakerColors = { "White", "Black", "Empty" };
+
+		// Win or lose
+		boolean hasWon = false;
 
 		// Code
-		String[] codeVakken = new String[4]; // 4 plekken geopent voor de codeVakken
-
-		for (int o = 0; o < 4; o++) { // 4 verschillende kleuren kiezen voor de code
-			int random = rd.nextInt(6); // Random number generator voor de kleuren
-			codeVakken[o] = codeKrakersKleuren[random]; // geeft nummer in de codeKrakersKleuren array in en herhaalt 4
-														// keer
+		String[] codeSlots = new String[4];
+		for (int o = 0; o < 4; o++) {
+			Random rd = new Random();
+			int random = rd.nextInt(6);
+			codeSlots[o] = codeGuesserColors[random];
 		}
 
-		// poging
-		for (int i = 0; i <= 10; i = i + 1) { // herhaalt het spel 10 keer
-			String[] inputs = new String[4]; // Maakt 4 plekken open voor ieder poging
-
-			for (int l = 0; l < inputs.length; l = l + 1) { // herhaalt voor ieder poging 4 kleuren die ingevuld mag
-															// worden
-				inputs[l] = sc.next(); // geeft de User inputs om kleuren in te vullen
+		// Attempt
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 10; i = i + 1) {
+			String[] inputs = new String[4];
+			for (int l = 0; l < inputs.length; l = l + 1) {
+				inputs[l] = sc.next();
 			}
-			// controle
+			// Result
 
-			String[] controles = new String[4];
+			String[] results = new String[4];
 
-			// game
-			boolean correct[] = new boolean[4];
+			// Game
+			boolean slotMatches[] = new boolean[4];
 
-			correct[0] = inputs[0].equalsIgnoreCase(codeVakken[0]);
+			slotMatches[0] = inputs[0].equalsIgnoreCase(codeSlots[0]);
 
-			if (correct[0]) {
-				controles[0] = codeMakersKleuren[0];
-			} else if (inputs[0].equalsIgnoreCase(codeVakken[1]) || inputs[0].equalsIgnoreCase(codeVakken[2])
-					|| inputs[0].equalsIgnoreCase(codeVakken[3])) {
-				controles[0] = codeMakersKleuren[1];
+			if (slotMatches[0]) {
+				results[0] = codeMakerColors[0];
+			} else if (inputs[0].equalsIgnoreCase(codeSlots[1]) || inputs[0].equalsIgnoreCase(codeSlots[2])
+					|| inputs[0].equalsIgnoreCase(codeSlots[3])) {
+				results[0] = codeMakerColors[1];
 			} else {
-				controles[0] = codeMakersKleuren[2];
+				results[0] = codeMakerColors[2];
 			}
-			System.out.println(controles[0]);
+			System.out.println(results[0]);
 
-			correct[1] = inputs[1].equalsIgnoreCase(codeVakken[1]);
+			slotMatches[1] = inputs[1].equalsIgnoreCase(codeSlots[1]);
 
-			if (correct[1]) {
-				controles[1] = codeMakersKleuren[0];
-			} else if (inputs[1].equalsIgnoreCase(codeVakken[0]) || inputs[1].equalsIgnoreCase(codeVakken[2])
-					|| inputs[1].equalsIgnoreCase(codeVakken[3])) {
-				controles[1] = codeMakersKleuren[1];
+			if (slotMatches[1]) {
+				results[1] = codeMakerColors[0];
+			} else if (inputs[1].equalsIgnoreCase(codeSlots[0]) || inputs[1].equalsIgnoreCase(codeSlots[2])
+					|| inputs[1].equalsIgnoreCase(codeSlots[3])) {
+				results[1] = codeMakerColors[1];
 			} else {
-				controles[1] = codeMakersKleuren[2];
+				results[1] = codeMakerColors[2];
 			}
-			System.out.println(controles[1]);
+			System.out.println(results[1]);
 
-			correct[2] = inputs[2].equalsIgnoreCase(codeVakken[2]);
+			slotMatches[2] = inputs[2].equalsIgnoreCase(codeSlots[2]);
 
-			if (correct[2]) {
-				controles[2] = codeMakersKleuren[0];
-			} else if (inputs[2].equalsIgnoreCase(codeVakken[0]) || inputs[2].equalsIgnoreCase(codeVakken[1])
-					|| inputs[2].equalsIgnoreCase(codeVakken[2])) {
-				controles[2] = codeMakersKleuren[1];
+			if (slotMatches[2]) {
+				results[2] = codeMakerColors[0];
+			} else if (inputs[2].equalsIgnoreCase(codeSlots[0]) || inputs[2].equalsIgnoreCase(codeSlots[1])
+					|| inputs[2].equalsIgnoreCase(codeSlots[2])) {
+				results[2] = codeMakerColors[1];
 			} else {
-				controles[2] = codeMakersKleuren[2];
+				results[2] = codeMakerColors[2];
 			}
-			System.out.println(controles[2]);
+			System.out.println(results[2]);
 
-			correct[3] = inputs[3].equalsIgnoreCase(codeVakken[3]);
+			slotMatches[3] = inputs[3].equalsIgnoreCase(codeSlots[3]);
 
-			if (correct[3]) {
-				controles[3] = codeMakersKleuren[0];
-			} else if (inputs[3].equalsIgnoreCase(codeVakken[0]) || inputs[3].equalsIgnoreCase(codeVakken[1])
-					|| inputs[3].equalsIgnoreCase(codeVakken[2])) {
-				controles[3] = codeMakersKleuren[1];
+			if (slotMatches[3]) {
+				results[3] = codeMakerColors[0];
+			} else if (inputs[3].equalsIgnoreCase(codeSlots[0]) || inputs[3].equalsIgnoreCase(codeSlots[1])
+					|| inputs[3].equalsIgnoreCase(codeSlots[2])) {
+				results[3] = codeMakerColors[1];
 			} else {
-				controles[3] = codeMakersKleuren[2];
+				results[3] = codeMakerColors[2];
 			}
-			System.out.println(controles[3]);
+			System.out.println(results[3]);
 
-			for (int t = 0; t < 4; t++) {
+			hasWon = (slotMatches[0] && slotMatches[1] && slotMatches[2] && slotMatches[3]);
 
-				boolean sum = (correct[0] && correct[1] && correct[2] && correct[3]);
-
-				if (sum) {
-					i = 11;
-					System.out.println("Gefeliciteert!! U heeft gewonnen!");
-				}
+			if (hasWon) {
+				System.out.println("Congratulations!! You have won!");
+				break;
 			}
 		}
-		System.out.println("Jammer! Je hebt verloren!");
+		if (hasWon == false) {
+			System.out.print("You have lost! Better luck next time!" + " \nThe code was: ");
+			for (String codeVak : codeSlots) {
+				System.out.print(codeVak + " ");
+			}
+		}
 		sc.close();
 	}
 }
